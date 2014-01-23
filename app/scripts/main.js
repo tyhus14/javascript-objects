fa5e8723afb32a9da4b409cc666454618e2d2bf1
tyhus14 <huston.tyler3@gmail.com>
2014-01-23 00:17:44 -0500

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+
+
+var Billy = {
+	legs: 2,
+	ears: 2,
+	hobbies: ['fighting', 'drinking', 'fishing'],
+	blackeye: false,
+
+	punches: function(person){
+		person.blackeye = true
+		return "Billy punched!"
+	}
+};
+
+
+var Johnny = {
+	legs: 2,
+	ears: 2,
+	hobbies: ['singing', 'painting', 'running'],
+	blackeye: false,
+	
+};
+
+var Shane = {
+	legs: 2,
+	ears: 2,
+	hobbies: ['swimming', 'eating', 'golfing'],
+	blackeye: false
+
+};
